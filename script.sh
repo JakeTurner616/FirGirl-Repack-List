c26cfdf4ba56b0140c2fcca8b344f43d60a189a8
JakeTurner616 <jake@serverboi.org>
2022-09-29 15:00:38 -0600

    Added comments

--- script.sh
+++ script.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
-mkdir $(pwd)/links 
+#this script will break if changes are made to the pagination system within: https://fitgirl-repacks.site/all-my-repacks-a-z
+#depends:
+apt-get install curl -y
 apt-get install lynx -y
+#{1..x} where x is the max page depth within pagination: https://fitgirl-repacks.site/all-my-repacks-a-z
 for page in {1..61} 
 do
+#download source and run tests to turnicate:
   rm -rf test.html && rm -rf fitsource.txt && rm -rf test2.html
   curl -sS https://fitgirl-repacks.site/all-my-repacks-a-z/?lcp_page0=$page#lcp_instance_0 >> fitsource.txt && wait 
   sed -n "/entry-content\">/,/lcp_paginator/p" fitsource.txt >> test.html && sed 's/<\/a><\/li><\/ul><ul class=.*//g' test.html >> test2.html
+#parse local html file with lynx:
   lynx -listonly -nonumbers --dump ~/test2.html >> RepackList.txt
 done
+#cleanup:
+rm -rf test.html && rm -rf fitsource.txt && rm -rf test2.html
