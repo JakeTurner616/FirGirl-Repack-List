ae0743508b50920fba96fda12784f346bafaadbb
JakeTurner616 <jake@serverboi.org>
2022-09-29 14:50:04 -0600

    Update script.sh

--- script.sh
+++ script.sh
@@ -6,5 +6,5 @@ do
   rm -rf test.html && rm -rf fitsource.txt && rm -rf test2.html
   curl -sS https://fitgirl-repacks.site/all-my-repacks-a-z/?lcp_page0=$page#lcp_instance_0 >> fitsource.txt && wait 
   sed -n "/entry-content\">/,/lcp_paginator/p" fitsource.txt >> test.html && sed 's/<\/a><\/li><\/ul><ul class=.*//g' test.html >> test2.html
-  lynx -listonly -nonumbers --dump ~/test2.html >> links.txt 
+  lynx -listonly -nonumbers --dump ~/test2.html >> RepackList.txt
 done
