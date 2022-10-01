62ee45f8a440474c2d36209ea6aadb8504e23b55
JakeTurner616 <jake@serverboi.org>
2022-10-01 02:05:16 -0600

    Added comments

--- script.sh
+++ script.sh
@@ -15,24 +15,28 @@ do
 done
 #cleanup:
 rm -rf test.html && rm -rf fitsource.txt && rm -rf test2.html
-#write out Download Mirror list to ~/links/fitgirl-repacks.site
-rm -rf ~/links/fitgirl-repacks.site
+#write out download mirror list to ~/links/fitgirl-repacks.site
 touch ~/magnet.txt
 file=$(cat RepackList.txt)
+#Given a RepackList.txt for each line:
 for line in $file
 do
     pagename=$(cat RepackList.txt | sed -r 's/^.{29}//')
     curl -sS "$line" > pagesource.txt
+#remove "https://" and add to env vairble "file" to be used in file structure later
     file=$(echo "$line" |sed 's/https\?:\/\///') 
+#make ~/links and touch magnet files with the name of the page extracted from RepackList.txt
     mkdir -p ~/links/"$file" && touch ~/links/"$file"magnet.html
+#extract source for download mirror list
     sed -n "/<h3>Download Mirrors<\/h3>/,/<h3>Screenshots (Click to enlarge)<\/h3>/p" pagesource.txt > ~/links/"$file"magnet.html
     echo $(lynx -listonly -nonumbers --dump ~/links/"$file"magnet.html) > ~/links/"$file"magnet.html
-    touch ~/links/"$file"magnet.txt 
+#rearrange temp0 files and write out a magnet.txt file
     mv ~/links/"$file"magnet.html ~/links/"$file"magnet.txt
     sed 's/ /\n/g' ~/links/"$file"magnet.txt > ~/links/"$file"magnet0.txt
     rm -rf sed 's/ /\n/g' ~/links/"$file"magnet.txt
     mv ~/links/"$file"magnet0.txt ~/links/"$file"magnet.txt
     rm -rf sed 's/ /\n/g' ~/links/"$file"magnet0.txt
+#change ownership
     sudo chown -R $(whoami) ~/links
 done
 #cleanup
