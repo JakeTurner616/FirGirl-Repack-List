31ff07eb5eae16962e21f31ce8397f3490d29ea3
JakeTurner616 <jake@serverboi.org>
2022-10-08 01:26:58 -0600

    Update RepackUpdater.sh

--- RepackUpdater.sh
+++ RepackUpdater.sh
@@ -5,6 +5,7 @@
 #depends:
 apt-get install curl -y
 apt-get install lynx -y
+rm -rf RepackList.txt
 #{1..x} where x is the max page depth within pagination: https://fitgirl-repacks.site/all-my-repacks-a-z
 for page in {1..61} 
 do
@@ -13,7 +14,7 @@ do
   curl -sS https://fitgirl-repacks.site/all-my-repacks-a-z/?lcp_page0=$page#lcp_instance_0 >> fitsource.txt && wait 
   sed -n "/entry-content\">/,/lcp_paginator/p" fitsource.txt >> test.html && sed 's/<\/a><\/li><\/ul><ul class=.*//g' test.html >> test2.html
 #parse local html file with lynx:
-  lynx -listonly -nonumbers --dump ~/test2.html > RepackList.txt
+  lynx -listonly -nonumbers --dump ~/test2.html >> RepackList.txt
 done
 #cleanup:
 rm -rf test.html && rm -rf fitsource.txt && rm -rf test2.html
