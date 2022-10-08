86980939b2adf03ef0e5d73fe98a1cf6fb427c9f
JakeTurner616 <jake@serverboi.org>
2022-10-08 01:21:57 -0600

    overwrite output file for automation

--- RepackUpdater.sh
+++ RepackUpdater.sh
@@ -13,7 +13,7 @@ do
   curl -sS https://fitgirl-repacks.site/all-my-repacks-a-z/?lcp_page0=$page#lcp_instance_0 >> fitsource.txt && wait 
   sed -n "/entry-content\">/,/lcp_paginator/p" fitsource.txt >> test.html && sed 's/<\/a><\/li><\/ul><ul class=.*//g' test.html >> test2.html
 #parse local html file with lynx:
-  lynx -listonly -nonumbers --dump ~/test2.html >> RepackList.txt
+  lynx -listonly -nonumbers --dump ~/test2.html > RepackList.txt
 done
 #cleanup:
 rm -rf test.html && rm -rf fitsource.txt && rm -rf test2.html
